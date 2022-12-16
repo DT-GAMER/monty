ee5003856dbe3cfaf7efcb06ebec825c0183aad1
Abakpa Dominic <abakpad82@gmail.com>
2022-12-16 08:49:03 +0800

    push.c

--- push.c
+++ push.c
@@ -1,73 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include "monty.h"
 
 /**
-
- * f_push - add node to the stack
-
- * @head: stack head
-
- * @counter: line_number
-
- * Return: no return
-
+ * push - push element into the stack
+ * @stack: stack given by main
+ * @line_cnt: amount of lines
+ *
+ * Return: void
  */
-
-void f_push(stack_t **head, unsigned int counter)
-  
+void push(stack_t **stack, unsigned int line_cnt)
 {
-  
-  int n, j = 0, flag = 0;
-  
+	char *n = global.argument;
 
-  
-  if (bus.arg)
-    
-    {
-      
-      if (bus.arg[0] == '-')
-	
-	j++;
-      
-      for (; bus.arg[j] != '\0'; j++)
-	
+	if ((is_digit(n)) == 0)
 	{
-	  
-	  if (bus.arg[j] > 57 || bus.arg[j] < 48)
-	    
-	    flag = 1; }
-      
-      if (flag == 1)
-	
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-	  
-	  fclose(bus.file);
-	  
-	  free(bus.content);
-	  
-	  free_stack(*head);
-	  
-	  exit(EXIT_FAILURE); }}
-  
-  else
-    
-    { fprintf(stderr, "L%d: usage: push integer\n", counter);
-      
-      fclose(bus.file);
-      
-      free(bus.content);
-      
-      free_stack(*head);
-      
-      exit(EXIT_FAILURE); }
-  
-  n = atoi(bus.arg);
-  
-  if (bus.lifi == 0)
-    
-    addnode(head, n);
-  
-  else
-    
-    addqueue(head, n);
-  
+		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
+		exit(EXIT_FAILURE);
+	}
+
+	if (global.data_struct == 1)
+	{
+		if (!add_node(stack, atoi(global.argument)))
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (!queue_node(stack, atoi(global.argument)))
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
 }
